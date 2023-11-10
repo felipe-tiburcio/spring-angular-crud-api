70df210df346f645e6d812daff5d4028fabc4daa
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-11-10 16:17:19 -0300

    post method to create customers working

--- src/main/java/com/ftatech/api/controller/Controller.java
+++ src/main/java/com/ftatech/api/controller/Controller.java
@@ -1,14 +1,30 @@
 package com.ftatech.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ftatech.api.model.Customer;
+import com.ftatech.api.repository.Repository;
 
 @RestController
 public class Controller {
-    
+
+    @Autowired
+    private Repository repository;
+
     @GetMapping("/")
     public String Hello() {
-        return "Hello world!";
+        return "Hello!";
+    }
+    
+    @PostMapping("/")
+    public Customer CreateCustomer(@RequestBody Customer customer) {
+        return repository.save(customer);
     }
 
 }
