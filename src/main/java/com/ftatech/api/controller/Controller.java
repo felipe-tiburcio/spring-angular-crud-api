61e4ab59fd7f7f9ef8aa0526ce9aca63b8671fc1
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-11-10 16:45:06 -0300

    update and delete methods ok

--- src/main/java/com/ftatech/api/controller/Controller.java
+++ src/main/java/com/ftatech/api/controller/Controller.java
@@ -3,13 +3,17 @@ package com.ftatech.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ftatech.api.model.Customer;
 import com.ftatech.api.repository.Repository;
+
 
 @RestController
 public class Controller {
@@ -25,6 +29,16 @@ public class Controller {
     @PostMapping("/")
     public Customer CreateCustomer(@RequestBody Customer customer) {
         return repository.save(customer);
+    }
+
+    @PutMapping("/")
+    public Customer EditCustomer(@RequestBody Customer customer) {
+        return repository.save(customer);
+    } 
+
+    @DeleteMapping("/{id}")
+    public void DeleteCustomer(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
 }
