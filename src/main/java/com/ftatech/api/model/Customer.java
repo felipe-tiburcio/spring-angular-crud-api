8c0be2a814b9685b08f20b94c5b036f35e4e72f9
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-11-10 15:45:23 -0300

    entity customer mapped by jpa and database connection working

--- src/main/java/com/ftatech/api/model/Customer.java
+++ src/main/java/com/ftatech/api/model/Customer.java
@@ -1,6 +1,22 @@
 package com.ftatech.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Customers")
+@Getter
+@Setter
 public class Customer {
-    private Long id;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int age;
+    private String city;
 }
--- src/main/java/com/ftatech/api/repository/Repository.java
+++ src/main/java/com/ftatech/api/repository/Repository.java
@@ -1,8 +1,4 @@
 package com.ftatech.api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ftatech.api.model.Customer;
-
-public interface Repository extends JpaRepository<Customer, Long> {
+public interface Repository {
 }
