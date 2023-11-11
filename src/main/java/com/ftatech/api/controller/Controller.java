a6845faf0c5ac4234a4bdf6b62f99ddd203eb79f
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-11-10 22:04:41 -0300

    cross origin issue solved

--- src/main/java/com/ftatech/api/controller/Controller.java
+++ src/main/java/com/ftatech/api/controller/Controller.java
@@ -3,6 +3,7 @@ package com.ftatech.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,8 @@ import com.ftatech.api.repository.Repository;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
-
     @Autowired
     private Repository repository;
 
@@ -40,5 +41,4 @@ public class Controller {
     public void DeleteCustomer(@PathVariable Long id) {
         repository.deleteById(id);
     }
-
 }
